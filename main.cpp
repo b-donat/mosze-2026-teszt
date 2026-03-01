2f10b52059b06a42fe4927befb5847e4702c8e88
b-donat <238335147+b-donat@users.noreply.github.com>
2026-03-01 12:55:51 +0100

    Added comments to bugs

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // no '_' in 'NELEMENTS'
+    std::cout << '1-100 ertekek duplazasa' // wrong quotation, '' instead of "";
+    for (int i = 0;) // for loop missing ' i; i++'
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)  // for loop missing condition: i < N_ELEMENTS
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //missing ';'
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;  //missing initialization
+    for (int i = 0; i < N_ELEMENTS, i++) // ',' instead of ';'
     {
-        atlag += b[i]
+        atlag += b[i]  //missing ';'
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
